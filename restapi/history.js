6efc1cd643ee8c7e92c1f16c4e08a469fdac5dc7
Pascal Jacquemart <P99_Pascal@yahoo.fr>
2016-06-10 20:17:03 +1000

    Different history for different users

--- restapi/history.js
+++ restapi/history.js
@@ -1,6 +1,7 @@
 
 var MongoClient = require('mongodb').MongoClient;
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 // Connection URL
 var url = process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://ca693a8ccfa2cd3464d532afdfab31c5:bf7863d7a446ecb58603a177c5eeed3b@ds011314.mlab.com:11314/accedo';
@@ -10,6 +11,7 @@ module.exports = function (app) {
         if (!err) {
             console.log("Connected succesfully to server");
             app.use( bodyParser.json() ); // for JSON-encoded bodies
+            app.use( cookieParser() );
             registerHistoryHandler(app, db);
         }
     });
@@ -18,19 +20,31 @@ module.exports = function (app) {
 function registerHistoryHandler(app, db) {
     app.all('/api/history/*', function (req, res) {
 
+        // Utility
         function resolve(err, result) {
             res.send({"status": (err ? "failure" : "success")});
         }
+
+        function findUser(req) {
+            var namespace = {};
+            if (req && req.cookies && req.cookies.accedo) {
+                namespace = JSON.parse(req.cookies.accedo);
+            }
+            return namespace.userId;
+        }
         
-        var history = db.collection('history');
-        var keyword = req.path.slice(13); // trim '/api/history/'
+        var history, keyword, user;
+        history = db.collection('history');
+        keyword = req.path.slice(13); // trim '/api/history/'
+        user = findUser(req);
+
 
         if ((keyword == "new") && (req.method == "PUT")) {
             // Create
-            history.updateOne({"id": req.body.id}, {$set: req.body}, {upsert: true}, resolve);
+            history.updateOne({"id": req.body.id, "user": user}, {$set: req.body}, {upsert: true}, resolve);
         } else if ((keyword == "all") && (req.method == "GET")) {
             // Read
-            history.find({}, {_id: 0}).toArray(function(err, docs) {
+            history.find({"user": user}, {_id: 0, user: 0}).toArray(function(err, docs) {
                 if (!err) {
                     res.send({"status": "success",
                         "totalCount": docs.length,
@@ -42,10 +56,10 @@ function registerHistoryHandler(app, db) {
             });
         } else if (keyword && (req.method == "POST")) {
             // Update
-            history.updateOne({"id": keyword}, {$set: req.body}, resolve);
+            history.updateOne({"id": keyword, "user": user}, {$set: req.body}, resolve);
         } else if (keyword && (req.method == "DELETE")) {
             // Delete
-            history.deleteOne({"id": keyword}, resolve);
+            history.deleteOne({"id": keyword, "user": user}, resolve);
         } else {
             res.send({"status": "failure"});
         }
